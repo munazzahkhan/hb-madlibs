092184acc6c24039058e3213f7919905e4a71030
Munazzah Hassan Khan <munazzah.hassan@gmail.com>
2021-05-11 14:30:33 -0700

    Completed tasks

--- madlibs.py
+++ madlibs.py
@@ -42,6 +42,32 @@ def greet_person():
                            compliment=compliment)
 
 
+@app.route('/game')
+def show_madlib_form():
+    response = request.args.get("response")
+    print("response : ", response)
+    if response == "no":
+        return render_template("goodbye.html")
+    else:
+        return render_template("game.html")
+
+@app.route('/madlib')
+def show_madlib():
+    person_name = request.args.get("person")
+    fav_color = request.args.get("color")
+    given_noun = request.args.get("noun")
+    given_adj = request.args.get("adjective")
+
+    return render_template("madlib.html",
+                            person = person_name,
+                            color = fav_color,
+                            noun = given_noun,
+                            adjective = given_adj)
+    
+            
+
+    
+
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
